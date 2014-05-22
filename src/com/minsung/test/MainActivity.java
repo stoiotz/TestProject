ddb8f367bca1bc23ae22e717b93ecbab018a39aa
minsung <kms@tvstorm.com>
2014-05-22 12:33:39 +0900

    First commit

---
+++ src/com/minsung/test/MainActivity.java
@@ -0,0 +1,16 @@
+package com.minsung.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by minsung on 14. 5. 22.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
