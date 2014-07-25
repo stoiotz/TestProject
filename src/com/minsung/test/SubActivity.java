29f0c2c1af98e9c4b7af9f06b27b9c0674cfee85
minsung <kms@tvstorm.com>
2014-07-25 15:00:09 +0900

    Test 기본 틀

--- src/com/minsung/test/SubActivity.java
+++ src/com/minsung/test/SubActivity.java
@@ -5,59 +5,46 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 
 /**
  * Created by minsung on 14. 5. 22.
  */
-public class MainActivity extends Activity {
-    private static final String TAG = "TVS_KMS_" + MainActivity.class.getSimpleName();
-
+public class SubActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, SubActivity.class));
+                startActivity(new Intent(SubActivity.this, MainActivity.class));
             }
         });
-
-        ((Button)findViewById(R.id.button)).setText(getString(R.string.test, 7));
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d("TVS_KMS", "onResume()");
+        Log.d("TVS_KMS", "SubActivity.onResume()");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d("TVS_KME", "onStart()");
+        Log.d("TVS_KME", "SubActivity.onStart()");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d("TVS_KMS", "onPause()");
+        Log.d("TVS_KMS", "SubActivity.onPause()");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d("TVS_KMS", "onStop()");
-    }
-
-    @Override
-    public void finish() {
-        super.finish();
-        Log.d("TVS_KMS", "finish()");
-        overridePendingTransition(0, 0);
+        Log.d("TVS_KMS", "SubActivity.onStop()");
     }
 }
